737364eb93c9c3087df7d19cf7d85a723161d7c3
AbdulMalik <abdumaliksharipov@gmail.com>
2020-07-05 00:20:10 +0500

    Fetching API part1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState,useEffect} from 'react';
 import Header from "./components/ui/Header"
+import CharacterGrid from './components/characters/CharacterGrid'
 import axios from "axios";
 import './App.css';
 
@@ -13,6 +14,8 @@ const App=()=> {
       const result=await axios(`https://www.breakingbadapi.com/api/characters/`)
 
       console.log(result.data)
+      setItems(result.data)
+      setLoading(false)
     }
 
     fetchItems();
@@ -24,6 +27,7 @@ const App=()=> {
 
       <h1>Assalaamu aleykom</h1>
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
