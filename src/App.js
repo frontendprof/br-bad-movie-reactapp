c3929bb73bbc20182dad38875ba3ed9c7511bb84
AbdulMalik <abdumaliksharipov@gmail.com>
2020-07-04 23:58:53 +0500

    Getting hooks onboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import Header from "./components/ui/Header"
+import axios from "axios";
 import './App.css';
 
 const App=()=> {
+
+  const [items,setItems] = useState([])
+  const [isLoading,setLoading] = useState(true)
+
+  
   return (
     <div className="container">
+
       <h1>Assalaamu aleykom</h1>
+      <Header />
     </div>
   );
 }
