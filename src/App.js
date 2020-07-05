5f3987c25a01cbf2b0ded7854114bccd5cafc9ef
AbdulMalik <abdumaliksharipov@gmail.com>
2020-07-05 09:56:15 +0500

    Getting search bar function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import Header from "./components/ui/Header"
 import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 import axios from "axios";
 import './App.css';
 
@@ -8,17 +9,18 @@ const App=()=> {
 
   const [items,setItems] = useState([])
   const [isLoading,setLoading] = useState(true)
+  const [query, setQuery]=useState('')
 
   useEffect(()=>{
     const fetchItems = async ()=>{
-      const result=await axios(`https://www.breakingbadapi.com/api/characters/`)
+      const result=await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
       setItems(result.data)
       setLoading(false)
     }
 
     fetchItems();
-  })
+  },[query])
 
   
   return (
@@ -26,6 +28,7 @@ const App=()=> {
 
       <h1>Assalaamu aleykom</h1>
       <Header />
+      <Search getQuery={q=>setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -12,19 +12,19 @@ const CharacterItem = ({item}) => {
                     <h1>{item.name}</h1>
                     <ul>
                         <li>
-                            <strong>Actor Name:</strong>{item.portrayed}
+                            <strong>Actor Name: </strong>  {item.portrayed}
                         </li>
 
                         <li>
-                            <strong>Nickname:</strong>{item.nickname}
+                            <strong>Nickname: </strong> {item.nickname}
                         </li>
 
                         <li>
-                            <strong>Birthday:</strong>{item.birthday}
+                            <strong>Birthday: </strong> {item.birthday}
                         </li>
 
                         <li>
-                            <strong>Status:</strong>{item.status}
+                            <strong>Status: </strong> {item.status}
                         </li>
                     </ul>
                 </div>
